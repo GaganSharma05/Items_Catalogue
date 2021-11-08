c48632dea34bf1cd0dd652690898b8598675b267
GaganSharma05 <gagan_sh@outlook.com>
2021-11-08 18:37:31 +0530

    day4 changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart' ;
-//import 'package:flutter_catalogue/home.dart';
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_catalogue/pages/home.dart';
 import 'package:flutter_catalogue/pages/login_page.dart';
-import 'package:flutter_catalogue/'
+import 'package:flutter_catalogue/utils/MyRoutes.dart';
 
 void main() {
   runApp(OTLogin());
@@ -11,12 +12,14 @@ class OTLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.indigo),
-        initialRoute: "\login",
-        routes: {
-          "/": (context) => Login_Page(),
-          //  "/login": (context) => Login_Page()
-        });
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        "/": (context) => Login_Page(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => Login_Page()
+      },
+    );
   }
 }
