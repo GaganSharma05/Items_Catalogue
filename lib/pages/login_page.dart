ef4e47ae412d7021dd6169a21cf996671498944b
GaganSharma05 <gagan_sh@outlook.com>
2021-11-08 22:00:03 +0530

    assets change

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,7 +9,7 @@ class Login_Page extends StatelessWidget {
         child: Column(
           children: [
             Image.asset(
-              "\Assets\Authentication.png",
+              "/assets/Authentication.png",
               fit: BoxFit.cover,
             ),
             SizedBox(
