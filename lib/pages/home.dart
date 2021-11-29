410e72ec37a6cfd80688b198973fbfbc286e8eab
GaganSharma05 <gagan_sh@outlook.com>
2021-11-29 17:42:26 +0530

    Changes on home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
 
 class Homepage extends StatelessWidget {
-  final String name = "Vegetables";
+  final String name = "Gagan";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Suprabhat -- Welcome to Fresh World"),
+        title: Text("Onlne Tutorial"),
       ),
       body: Center(
-        child: Container(child: Text("Welcome to Online shopping $name")),
+        child: Container(child: Text("Welcome to Online tutorial $name")),
       ),
       drawer: MyDrawer(),
     );
