16e17728d30c9484217298997ae5432b5e9b094a
GaganSharma05 <gagan_sh@outlook.com>
2021-12-05 13:28:50 +0530

    Catalog

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,34 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_catalogue/modules/catalogue.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final String name = "Gagan";
+
+  @override
+  void initState() {
+    super.initState();
+    loaddata();
+  }
+
+  loaddata() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
